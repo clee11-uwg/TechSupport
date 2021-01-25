74e3ffd5571a8c574bd75868843c48cd086db2a5
leecodya <lee.codya@gmail.com>
2021-01-25 15:53:46 -0500

    iniital creation of IncidentController

---
+++ Controller/IncidentController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupport.Controller
+{
+    public class IncidentController
+    {
+         
+    }
+}
