c25934c36241b8b2b4e499aa0bfb97f79f557804
leecodya <lee.codya@gmail.com>
2021-01-25 15:51:10 -0500

    added rest of the functionality needed to create the Incident model class

--- Model/Incident.cs
+++ Model/Incident.cs
@@ -8,13 +8,39 @@ namespace TechSupport.Model
 {
     public class Incident
     {
+        /// <summary>
+        /// Incident class to create an Incident object
+        /// </summary>
         public string Title { get; }
         public string Description { get; }
         public int CustomerID { get; }
 
+        /// <summary>
+        /// 3 parameter constructor for Incident class
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="description"></param>
+        /// <param name="customerID"></param>
         public Incident(string title, string description, int customerID)
         {
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentException("Incident title cannot be null or empty");
+            }
 
+            if (string.IsNullOrEmpty(description))
+            {
+                throw new ArgumentException("Incident description cannot be null or empty");
+            }
+
+            if (customerID <= 0)
+            {
+                throw new ArgumentException("customerID", "Incident's customerID must be more than 0");
+            }
+
+            this.Title = title;
+            this.Description = description;
+            this.CustomerID = customerID;
         }
     }
 }
