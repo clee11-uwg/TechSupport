b2483d3fff6f29add64ed0b1600a834fb1f36c23
leecodya <lee.codya@gmail.com>
2021-01-25 15:43:22 -0500

    added controller folder, incidentDAL file to house the incident data, and Incidient model class

---
+++ DAL/IncidentDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupport.DAL
+{
+    public class IncidentDAL
+    {
+        private static List<Incident> _incidents = new List<Incident>
+        {
+            new Incident("Site down", "The website is down", 5),
+            new Incident("Can't close program", "I am unable to close the program", 2)
+        };
+    }
+}
---
+++ Model/Incident.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupport.Model
+{
+    public class Incident
+    {
+        public string Title { get; }
+        public string Description { get; }
+        public int CustomerID { get; }
+
+        public Incident(string title, string description, int customerID)
+        {
+
+        }
+    }
+}
